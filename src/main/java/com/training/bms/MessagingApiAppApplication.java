75396a87adf204caa684b2b164c733786af25f39
Tanishq Kashyap <tankashy@dhcp-10-120-156-77.wifi.oracle.com>
2024-07-12 14:21:03 +0530

    initial commit

---
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -0,0 +1,23 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+@SpringBootApplication
+@RestController
+public class MessagingApiAppApplication {
+
+	@GetMapping("/")
+	public String printGreetings() {
+		return "Hi Oracle";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApiAppApplication.class, args);
+		
+		System.out.println("App started");
+	}
+
+}
